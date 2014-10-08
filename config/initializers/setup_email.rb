be190fc371bc9d50653702daa9e0f15663e58c3f
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-10-08 06:21:50 -0500

    Republish Secrets

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,12 +1,12 @@
 if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :authentication => :plain,
-    user_name: Rails.application.secrets.gmail_username,
-    password: Rails.application.secrets.gmail_password,
-    :domain         => 'gabrielsarmiento.com',
-    :enable_starttls_auto => true
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
   }
 end
