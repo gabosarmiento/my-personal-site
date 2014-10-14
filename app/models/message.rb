8ea707cf90dbc22205a417c93857b80e4b2bed53
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-10-14 18:09:41 -0500

    Hot fix form fields validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,10 +6,11 @@ class Message
 
   attr_accessor :name, :email, :body, :ideas
 
-  validates :name, :email, :body, :presence => true
-  validates :name, length: { minimum: 1, maximum: 200 }
-  validates :email, :format => { :with => %r{/\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i} }, :allow_blank => true
-  validates :ideas, length: {minimum: 1, maximum: 10000 } , :allow_blank => true
+  validates_presence_of :name, :email, :body, :ideas
+  validates :name, length: { minimum: 2, maximum: 150 }
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :body, length: {minimum: 1, maximum: 10000 }
+  validates :ideas, length: {minimum: 1, maximum: 10000 }
   
   def initialize(attributes = {})
     attributes.each do |name, value|
