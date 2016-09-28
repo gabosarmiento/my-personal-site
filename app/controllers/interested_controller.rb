e3d78c545ed8735f92dce22a4119cb75dc3fefdb
gabosarmiento <gabrielsarmiento@gmail.com>
2016-09-28 00:35:44 -0500

    update copies in home

--- app/controllers/interested_controller.rb
+++ app/controllers/interested_controller.rb
@@ -2,10 +2,10 @@ class InterestedController < ApplicationController
 
   def create
     @message = Message.new(params[:message])
-    
+
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => "Message sent succesfully.")
+      redirect_to(root_path, :notice => "Message sent succesfully. Please check your mail in a couple of minutes. Thanks for your patience.")
     else
       redirect_to(root_path, :alert => "I'm sorry try again and please check the application form.")
     end
